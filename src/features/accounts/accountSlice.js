2869e4f340358fb19ed1ce8f7f7e984a9c32bb7b
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-04 13:11:19 +0000

    improve file structure - screate separate store slices

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,64 @@
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+//reducer is a function that takes the current state and an action and returns a new state
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+// store.dispatch({ type: "account/deposit", payload: 100 });
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "pay off debt" },
+// });
+// console.log(store.getState());
+
+//action creators are functions that return actions
+//they are used to avoid typos and to make the code more readable
+
+export function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+export function payLoan() {
+  return { type: "account/payLoan" };
+}
---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,52 @@
+const initalStateCustomer = {
+  fullName: "",
+  nationalId: "",
+  createdAt: "",
+};
+
+//reducer is a function that takes the current state and an action and returns a new state
+
+export default function customerReducer(state = initalStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    case "customer/updateNationalId":
+      return {
+        ...state,
+        nationalId: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalId) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalId, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return {
+    type: "customer/updateName",
+    payload: fullName,
+  };
+}
+
+export function updateNationalId(nationalId) {
+  return {
+    type: "customer/updateNationalId",
+    payload: nationalId,
+  };
+}
