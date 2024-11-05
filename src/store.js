6c39e6d450a71598bea292b2a3089120c240b4ef
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-05 18:46:51 +0000

    convert redux codebase to redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,19 +1,29 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+// These imports are for CLASSIC REDUX code prior to using Redux Toolkit
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import { thunk } from "redux-thunk";
+
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { composeWithDevTools } from "@redux-devtools/extension";
+
+//this is CLASSIC REDUX code prior to using Redux Toolkit
 
 //combineReducers is a function that takes an object of reducers and returns a new reducer
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
 
-//store is a place to store the state of the application
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// //store is a place to store the state of the application
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
+});
 
 export default store;
