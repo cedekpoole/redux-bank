7b5332809c96eafb97fa09454fa81c4f787e93e1
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-05 14:39:39 +0000

    create balance display component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import Container from "./components/Container";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
 
 function App() {
   const hasAccount = useSelector((store) => store.customer.hasAccount);
@@ -17,6 +18,7 @@ function App() {
         ) : (
           <>
             <Customer />
+            <BalanceDisplay />
             <AccountOperations />
           </>
         )}
---
+++ src/features/accounts/BalanceDisplay.jsx
@@ -0,0 +1,24 @@
+import { connect } from "react-redux";
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "GBP",
+  }).format(value);
+}
+
+function BalanceDisplay({ balance }) {
+  return (
+    <div className=" my-2 p-2 bg-gray-100 w-44 text-center">
+      {formatCurrency(balance)}
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
