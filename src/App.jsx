5a109045fe1b8e16adc0dc5ab3bc30b2b6b92353
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-05 13:24:20 +0000

    conditionally render customer creation form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useSelector } from "react-redux";
 import Header from "./components/Header";
 import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
 
 function App() {
+  const hasAccount = useSelector((store) => store.customer.hasAccount);
+
   return (
     <>
       <div className="container mx-auto mt-5 font-inter text-[#1E272D]">
         <Header />
-        <CreateCustomer />
-        <Customer />
+        {!hasAccount ? (
+          <CreateCustomer />
+        ) : (
+          <>
+            <Customer />
+            <AccountOperations />
+          </>
+        )}
       </div>
     </>
   );
---
+++ src/features/accounts/AccountOperations.jsx
@@ -0,0 +1,5 @@
+function AccountOperations() {
+  return <div></div>;
+}
+
+export default AccountOperations;
